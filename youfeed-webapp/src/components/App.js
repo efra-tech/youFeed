f53adf1605944e396f96830a8d33b0b281489734
efra-tech <eahsan@uw.edu>
2023-07-19 20:55:55 -0700

    imgs added

--- youfeed-webapp/src/components/App.js
+++ youfeed-webapp/src/components/App.js
@@ -1,13 +1,15 @@
-import img from './imgs/taco.png';
+import img from '../imgs/logo.png';
+import wood from '../imgs/wood.jpg';
 
 export default function App(props) {
   return (
     <div className='container'>
-      <div className='logo-font'>LOGO</div>
+      <div className='a' >
+        <img src={img} alt='logo' style={{width: '20%', marginLeft: '40%'}} />
+      </div>
       <div className='container-main' style={{marginTop: '8%'}}>
         <div className='d-flex'>
-          {/* <img sr={img} alt='wireframe stack' /> */}
-          <div style={{backgroundColor: 'pink', width: '20em', height: '20em', marginLeft: '20%'}}></div>
+          <div style={{border: '15px solid black', width: '20em', height: '20em', marginLeft: '20%'}}></div>
           <div className='align-content-center align-self-center text-center'>
             <h1>MAIN HEADER</h1>
             <p className='vision-stmt' style={{color:"#777575", fontSize: '0.9rem'}}>SUBHEADER</p>
